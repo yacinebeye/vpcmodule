a8e4cc2712f9178f6a700181eea3c058db82bf1d
yacinebeye <yacine.beye@cloudreach.com>
2022-09-08 15:57:31 -0400

    updates

--- intnatgtw.tf
+++ intnatgtw.tf
@@ -1,3 +1,5 @@
+#INTERNET GTW 
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
@@ -10,6 +12,19 @@ resource "aws_eip" "nat_eip" {
   vpc = true
 }
 
+#NAT GATEWAY
+
+resource "aws_nat_gateway" "natgtw" {
+    allocation_id = aws_eip.nat_eip.id
+    connectivity_type = "public"
+    subnet_id = aws_subnet.public.id
+
+  tags = {
+    Name = "dev-env-natigw"
+  }
+  depends_on = [aws_internet_gateway.igw]
+}
+
 #ROUTETABLES
 
 resource "aws_route_table" "nat_route_table" {
@@ -17,7 +32,7 @@ resource "aws_route_table" "nat_route_table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat_gw.id
+    gateway_id = aws_nat_gateway.natgtw.id
   }
 
   tags = {
@@ -25,11 +40,24 @@ resource "aws_route_table" "nat_route_table" {
   }
 }
 
+resource "aws_route_table" "internet_route_table" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "internet-route-table"
+  }
+}
+
 # ASSOCIATE ROUTE TABLE -- PRIVATE LAYER
-#resource "aws_route_table_association" "internet_route_table_association_private" {
-#  subnet_id      = aws_subnet.private.id
-#  route_table_id = aws_route_table.nat_route_table.id
-#}
+resource "aws_route_table_association" "internet_route_table_association_private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.nat_route_table.id
+}
 
 # ASSOCIATE ROUTE TABLE -- PUBLIC LAYER
 resource "aws_route_table_association" "internet_route_table_association_public" {
